4e72a54fb80f344a55f16975ab8d37dd2542002d
Светлана <skarpuhova@mail.ru>
2023-03-05 20:37:58 +0300

    Task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+if (a == b*b)Console. WriteLine("Yes");
+else Console. WriteLine("No");
+
+
+// Console.WriteLine("Hello, World!");
